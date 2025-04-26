4cf3cb23c29a1accd4608d83d319d73ecfc4eafc
Jhons Puma <jhonpuma100@gmail.com>
2025-04-26 14:37:41 -0500

    first commit

---
+++ src/main/java/edu/cibertec/appstorecomputer/AppstorecomputerApplication.java
@@ -0,0 +1,13 @@
+package edu.cibertec.appstorecomputer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppstorecomputerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppstorecomputerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cibertec/appstorecomputer/AppstorecomputerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cibertec.appstorecomputer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppstorecomputerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
